766d86e0f96d2685efe107a266c3b0e6592c4e82
luisfg <luisfajardog7@gmail.com>
2026-03-26 17:58:50 -0500

    se agrega una nueva funcion respondiendo al primer mensaje

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,9 +1,15 @@
 public class ejercicio1 {
     public static void main(String[] args){
         saludo();
+        saludo2();
     }
 
     public  static void saludo(){
     System.out.println("Como estas?...");
     }
+
+    public  static void saludo2(){
+    System.out.println("Bien y tu?...");
+    }
+
 }
